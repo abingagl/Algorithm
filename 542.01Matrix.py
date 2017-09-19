8dd1e3209783fcdc789006984dfd2a4d9d45b208
abingagl <bxzhang2@163.com>
2017-09-19 09:08:05 +0800

    day4

--- 542.01Matrix.py
+++ 542.01Matrix.py
@@ -4,4 +4,28 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
         """
-        
+        if not matrix:
+            return []
+        m, n = len(matrix), len(matrix[0])
+        queue = []
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    queue.append([i, j])
+                else:
+                    matrix[i][j] = 10000
+
+        dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        while queue != []:
+            curr = queue[0]
+            queue.remove(curr)
+            for d in dirs:
+                x = curr[0] + d[0]
+                y = curr[1] + d[1]
+                if x < 0 or x >= m or y < 0 or y >= n or \
+                        matrix[x][y] <= matrix[curr[0]][curr[1]] + 1:
+                    continue
+                queue.append([x, y])
+                matrix[x][y] = matrix[curr[0]][curr[1]] + 1
+
+        return matrix
